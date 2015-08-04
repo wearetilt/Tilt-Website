0a48d8c0a3d2c4af2b813ba2a09b97863e0f16ee
wearemiles <miles@wearetilt.com>
2015-08-04 14:09:27 +0100

    Carousel completed

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ menuButton.onclick = function(){
 }
 
 var allModules = document.getElementsByClassName('module');
-console.log(allModules);
 
 for (var i = 0; i < allModules.length; i++){
     var module = allModules[i];
@@ -29,4 +28,25 @@ for (var i = 0; i < allModules.length; i++){
         },
         offset: 200
     });
+}
+
+// Get all the carousel controls on the page and all the images on the page
+//TODO: Add a way where the controls are created based on the number of images
+//This might break centring
+var carouselImages = document.getElementsByClassName('carousel-image');
+var carouselControls = document.getElementsByClassName('carousel-control');
+
+// Loop through all the controls and add a click handler to all of them.
+for (var iterator = 0; iterator < carouselControls.length; iterator++){
+    var carouselControl = carouselControls[iterator];
+    carouselControl.onclick = function (){
+        //grab the ID from the carousel control
+        var imageToShow = this.getAttribute('ID').slice(-1);
+        //Target the image with the matching ID and expand it while hiding all the others
+        for(var iterator2 = 0; iterator2 < carouselImages.length; iterator2++){
+            carouselImage = carouselImages[iterator2];
+            carouselImage.style.height = 0;
+        }
+        document.getElementById('carousel-image-' + imageToShow).style.height = '100%';
+    }
 }
