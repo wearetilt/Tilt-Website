050dbaf51de4c534ee704e28c1719e9a8af7fad1
wearemiles <miles@wearetilt.com>
2015-09-16 09:45:16 +0100

    All pages templated, need populating

--- wp-content/themes/tilt-site/page-barclays-values.php
+++ wp-content/themes/tilt-site/page-barclays-values.php
@@ -11,8 +11,106 @@
  * @since Twenty Fifteen 1.0
  */
 
-get_header(); ?>
+get_header('motion'); ?>
 
-<p>BARCLAYS VALUES</p>
+    <div class="container container--half-top">
+        <div class="group-container">
+            <div class="group group--left">
+                <div class="module module--2-2">
+                    <div class="overlay area-dark">
+                        <div class="overlay-text">
+                            <p class="tag">Motion: Case Study</p>
+                            <h2>Barclays <br />
+                                <span class="light underlined">Integrity</span>
+                            </h2>
+                            <p class="sans-serif">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure culpa qui deserunt, esse impedit unde ex.</p>
+                        </div> <!-- /end overlay-text -->
+                    </div> <!-- /end overlay -->
+                    <div class="ratio" style="background-image: url('images/video-placeholder.jpg')">
+                    </div>
+                </div>
+            </div> <!-- /end group -->
+            <div class="group group--right">
+                <div class="module module--1-1"></div>
+                <div  class="module module--1-1"></div>
+                <div class="module module--2-1"></div>
+            </div> <!-- /end group -->
+        </div> <!-- /end group-container -->
+        <section class="text-section">
+            <h2>Welcome to Tilt</h2>
+            <div class="text-section__para">
+                <p class="first-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi a voluptatem maiores velit voluptate, asperiores vitae, quisquam ratione non laborum temporibus rem minima dolor, adipisci repudiandae aut numquam earum dignissimos? Iste a commodi enim voluptates, fuga amet omnis laborum saepe praesentium veniam asperiores quibusdam, magnam nesciunt reprehenderit ipsum totam aspernatur! Consequatur autem excepturi perferendis incidunt dolor culpa quod. Omnis, quos!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi qui, praesentium aliquam, possimus rem, vitae consequatur et dignissimos quibusdam quas dolores modi suscipit minus quis cumque! Distinctio totam molestiae porro! Deserunt ipsam voluptatibus esse qui quod praesentium iusto eveniet, culpa! Suscipit incidunt fugit illum, quaerat atque rerum, numquam illo quis, tenetur hic assumenda praesentium ad. Magni vero consequuntur eos nesciunt!</p>
+            </div>
+        </section> <!-- /end text-section -->
+    </div>
+
+    <div class="container container--no-padding">
+        <section>
+            <div class="group-container">
+                <div class="group group--left">
+                    <div class="module module--2-1 area-dark">
+                        <div class="module__text">
+                            <h2 class="underlined">The Process</h2>
+                            <p class="first-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos quod optio, delectus eaque temporibus, at ab placeat incidunt ad esse veritatis est, minima quidem animi omnis natus numquam illum. Repellat aliquid, natus amet quo. Quisquam voluptates magnam aspernatur itaque molestiae error debitis delectus sunt, est non pariatur assumenda accusantium vel maxime optio placeat sint enim explicabo voluptate asperiores natus.</p>
+                        </div>
+                    </div>
+                    <div class="module module--2-1"></div>
+                </div>
+                <div class="group group--right">
+                    <div class="module module--2-2"></div>
+                </div> <!-- /end group -->
+            </div> <!-- /end group-container -->
+        </section>
+    </div> <!-- /end container -->
+
+    <div class="container container--no-padding">
+        <section>
+            <div class="group-container">
+                <div class="group group--left">
+                    <div class="module module--2-1 area-dark">
+                        <div class="module__text">
+                            <h2 class="underlined">The Process</h2>
+                            <p class="first-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quos quod optio, delectus eaque temporibus, at ab placeat incidunt ad esse veritatis est, minima quidem animi omnis natus numquam illum. Repellat aliquid, natus amet quo. Quisquam voluptates magnam aspernatur itaque molestiae error debitis delectus sunt, est non pariatur assumenda accusantium vel maxime optio placeat sint enim explicabo voluptate asperiores natus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="group group--right">
+                    <div class="module module--1-1"></div>
+                    <div class="module module--1-1"></div>
+                </div> <!-- /end group -->
+            </div> <!-- /end group-container -->
+        </section>
+    </div> <!-- /end container -->
+
+    <div class="container container--no-padding">
+        <section>
+            <div class="group-container">
+                <div class="group group--right">
+                <div id="module-5" class="module module--2-2"></div>
+                </div>
+                <div class="group group--left">
+                    <div class="module module--2-1"></div>
+                    <div class="module module--2-1"></div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div class="container container--no-padding">
+        <section>
+            <div class="group-container">
+                <div class="group group--left">
+                <div id="module-5" class="module module--2-2"></div>
+                </div>
+                <div class="group group--right">
+                    <div class="module module--2-1"></div>
+                    <div class="module module--2-1"></div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
