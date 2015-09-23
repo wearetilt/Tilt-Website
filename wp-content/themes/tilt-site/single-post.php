7a3016b215289a1e8999747f31ab65d83edd8e9b
wearemiles <miles@wearetilt.com>
2015-09-23 09:41:10 +0100

    News posts started

--- wp-content/themes/tilt-site/single-post.php
+++ wp-content/themes/tilt-site/single-post.php
@@ -11,23 +11,54 @@ get_header(); ?>
 
 <div class="news-wrapper">
 	<div class="news-container">
+		<header id="news" class="work-item area-dark">
+		    <div class="container container--header">
+		        <div class="header-title">
+		            <p class="tag">News</p>
+		            <h1 class="underlined">Tilt Develop Fun Ecosystem games for pbs
+		            </h1>
+					<div>
+		            	<h2 class="light">3 August By Jonathan Helm</h2>
+						<h2>Share: Email Facebook Twitter</h2>
+					</div>
 
+		        </div>
+		    </div>
+		</header>
+		<div class="intro-text">
+			<p>Tilt have developed a series of advanced HTML5 games for Earth A New Wild, a new series currently airing on American television network PBS. Produced by National Geographic and Passion Planet, the series takes a fresh look at humankind’s relationship to the planet’s wildest places and most fascinating species.</p>
+		</div>
 	</div>
-	<div class="breakout">
+	<div class="container container--no-padding">
+		<div class="group-container">
+			<div class="group group--left">
+				<div class="module module--2-2">
 
+				</div>
+			</div>
+			<div class="group group--right">
+				<div class="module module--1-1"></div>
+				<div class="module module--1-1"></div>
+				<div class="module module--2-1"></div>
+			</div>
+		</div>
 	</div>
 	<div class="news-container">
-
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex illum nulla iste illo, voluptas itaque, eius? Temporibus provident perferendis vitae eaque eum dignissimos iure illum modi cum, molestiae delectus vel. Laboriosam similique vitae in consequuntur corporis aliquid necessitatibus quos atque, doloribus cupiditate dolor magnam beatae cumque dolore, tempore ad. Tempore mollitia iure a dolorem sed ratione enim nemo voluptate. Ipsam! Debitis asperiores repudiandae animi doloremque fugiat delectus, impedit ad, sint eaque, esse quisquam eum ab accusamus sunt nemo.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex illum nulla iste illo, voluptas itaque, eius? Temporibus provident perferendis vitae eaque eum dignissimos iure illum modi cum, molestiae delectus vel. Laboriosam similique vitae in consequuntur corporis aliquid necessitatibus quos atque, doloribus cupiditate dolor magnam beatae cumque dolore, tempore ad. Tempore mollitia iure a dolorem sed ratione enim nemo voluptate. Ipsam! Debitis asperiores repudiandae animi doloremque fugiat delectus, impedit ad, sint eaque, esse quisquam eum ab accusamus sunt nemo.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex illum nulla iste illo, voluptas itaque, eius? Temporibus provident perferendis vitae eaque eum dignissimos iure illum modi cum, molestiae delectus vel. Laboriosam similique vitae in consequuntur corporis aliquid necessitatibus quos atque, doloribus cupiditate dolor magnam beatae cumque dolore, tempore ad. Tempore mollitia iure a dolorem sed ratione enim nemo voluptate. Ipsam! Debitis asperiores repudiandae animi doloremque fugiat delectus, impedit ad, sint eaque, esse quisquam eum ab accusamus sunt nemo.</p>
 	</div>
 </div>
 
 <div class="img-container">
-
+	<img src="<?php echo get_template_directory_uri(); ?>/images/news-image.png" alt="">
 </div>
 
 <div class="group-container">
 	<a class="project-navigation" href="">< Previous Project</a>
 	<a class="project-navigation" href="">Next Project ></a>
-</div>	
+</div>
+
+</div>
 
 <?php get_footer(); ?>
