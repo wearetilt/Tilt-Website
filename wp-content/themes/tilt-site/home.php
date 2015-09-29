4f34da0176438764a323f646c06e9bdb2c37d4bf
wearemiles <miles@wearetilt.com>
2015-09-29 10:52:36 +0100

    Nav styling for news page

--- wp-content/themes/tilt-site/home.php
+++ wp-content/themes/tilt-site/home.php
@@ -107,9 +107,9 @@ get_header(); ?>
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+				'prev_text'          => __( '< Prev', 'twentyfifteen' ),
+				'next_text'          => __( 'Next >', 'twentyfifteen' ),
+				'screen_reader_text' => __( '' )
 			) );
 
 		// If no content, include the "No posts found" template.
