4a332d67de922671c6dfe6f2e45a72a2606b7895
wearemiles <miles@wearetilt.com>
2015-09-29 11:43:28 +0100

    Styling for news page

--- wp-content/themes/tilt-site/content-blog-post.php
+++ wp-content/themes/tilt-site/content-blog-post.php
@@ -19,11 +19,11 @@
 <a href="<?php echo get_permalink(get_the_id()); ?>">
 	<div id="post=<?php the_ID();?>" class="overlay area-dark">
 		<div class="overlay-text">
-			<p class="tag"><?php get_the_date(); ?></p>
+			<p class="tag"><?php echo get_the_date('F Y'); ?></p>
 			<h2><?php the_title( ); ?></h2>
 			<p class="sans-serif"><?php the_excerpt(); ?></p>
 		</div> <!-- /end overlay-text -->
 	</div> <!-- /end overlay -->
-	<div class="ratio" style="background-image: url('<?php $attachedImg; ?>')">
+	<div class="ratio" style="background-image: url('<?php echo $attachedImg[0]; ?>')">
 	</div>
 </a>
