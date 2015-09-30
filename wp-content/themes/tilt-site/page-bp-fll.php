02f0b8e8d3b4ce4fbc57791cc7dae822e12775ee
Jonathan Malyon <jon@wearetilt.com>
2015-09-30 12:57:38 +0100

    BP First Level Leaders population

--- wp-content/themes/tilt-site/page-bp-fll.php
+++ wp-content/themes/tilt-site/page-bp-fll.php
@@ -17,25 +17,25 @@
      <div class="monitor-holder">
          <img class="monitor centre-image" src="<?php echo get_template_directory_uri(); ?>/images/monitor.png" alt="">
          <div class="monitor-screen">
-             <img src="<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_imacsmall.jpg" alt="" style="width: 100%; height: 100%;">
+             <img src="<?php echo get_template_directory_uri(); ?>/images/work/web_fll/web_fll_imacsmall.jpg" alt="" style="width: 100%; height: 100%;">
          </div>
      </div>
      <div class="container container--header">
          <div class="header-title">
-             <p class="tag">Interactive</p>
-             <h1>Passion Pictures<br />
-                 <span class="light underlined">Earth - A New Wild</span>
+             <p class="tag">Web</p>
+             <h1>BP<br />
+                 <span class="light underlined">First Level Leaders</span>
              </h1>
-             <h2 class="light">Design | Illustration | DEVELOPMENT</h2>
+             <h2 class="light">Strategy, Design, Dev, Film & Motion</h2>
          </div>
          <div class="header-text">
              <div class="header-text__module header-text__module--padded">
                  <h2>The challenge</h2>
-                 <p class="first-para">Use the power of digital to help young people explore the ideas behind a National Geographic nature documentary.</p>
+                 <p class="first-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
              </div>
              <div class="header-text__module">
                  <h2>The Big Idea</h2>
-                 <p class="first-para">Create a batch of addictive, playful HTML5 micro-games that take students deep into the amazing worlds of vultures, wolves and sharks.</p>
+                 <p class="first-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
              </div>
          </div>
      </div>
@@ -45,10 +45,10 @@
      <section class="carousel">
          <img class="carousel-monitor centre-image" src="<?php echo get_template_directory_uri(); ?>/images/monitor.png" alt="">
          <div class="carousel-images">
-             <div id="carousel-image-1"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_01_imacsmall.jpg')"></div>
-             <div id="carousel-image-2"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_02_imacsmall.jpg')"></div>
-             <div id="carousel-image-3"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_03_imacsmall.jpg')"></div>
-             <div id="carousel-image-4"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_04_imacsmall.jpg')"></div>
+             <div id="carousel-image-1"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery/web_fll_gallery_01_imacsmall.jpg')"></div>
+             <div id="carousel-image-2"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery/web_fll_gallery_02_imacsmall.jpg')"></div>
+             <div id="carousel-image-3"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery/web_fll_gallery_03_imacsmall.jpg')"></div>
+             <div id="carousel-image-4"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery/web_fll_gallery_04_imacsmall.jpg')"></div>
          </div> <!-- /end carousel-images -->
          <div class="carousel-controls">
              <div id="carousel-control-1" class="carousel-control selected"></div>
@@ -66,16 +66,16 @@
                  <div class="module module--2-1 module--dark">
                      <div class="module__text">
                          <h2 class="underlined">What we did</h2>
-                         <p class="first-para">Earth - A New Wild is a nature documentary that shows young audiences that ecosystems often hang in a delicate balance and are easily disrupted by the actions of man. The producer, Passion Pictures, approached us to create an engaging digital experience to support the show. The original brief asked for a microsite with an overview of each show, but we saw an opportunity to do much more...</p>
+                         <p class="first-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                      </div>
                  </div>
                  <div class="module module--2-1">
- 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_01_mr.jpg')"></div>
+ 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/web_fll_01_mr.jpg')"></div>
                  </div>
              </div>
              <div class="group group--right">
                  <div class="module module--2-2">
- 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_02_ls.jpg')"></div>
+ 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/web_fll_02_ls.jpg')"></div>
                  </div>
              </div> <!-- /end group -->
          </div> <!-- /end group-container -->
@@ -88,16 +88,16 @@
              <div class="group group--left">
                  <div class="module module--2-1 module--dark">
                      <div class="module__text">
-                         <p>We created Ecosystem Explorer, a portal jam-packed with videos, games and infographics designed to take students deep into three thrilling ecosystems based on the lives of vultures, wolves and sharks. Built using Create JS and HTML5, the games work in any browser and feature original artwork, illustrations and sound effects created by our team.</p>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                      </div>
                  </div>
              </div>
              <div class="group group--right">
                  <div class="module module--1-1">
- 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_03_ss.jpg')"></div>
+ 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/web_fll_03_ss.jpg')"></div>
                  </div>
                  <div class="module module--1-1">
- 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_04_ss.jpg')"></div>
+ 	                <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/web_fll_04_ss.jpg')"></div>
                  </div>
              </div> <!-- /end group -->
          </div> <!-- /end group-container -->
@@ -108,10 +108,10 @@
      <section class="carousel">
          <img class="carousel-monitor centre-image" src="<?php echo get_template_directory_uri(); ?>/images/monitor.png" alt="">
          <div class="carousel-images">
-             <div id="carousel-image-1"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_01_imacsmall.jpg')"></div>
-             <div id="carousel-image-2"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_02_imacsmall.jpg')"></div>
-             <div id="carousel-image-3"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_03_imacsmall.jpg')"></div>
-             <div id="carousel-image-4"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/gallery/int_pbs_gallery_04_imacsmall.jpg')"></div>
+             <div id="carousel-image-1"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery2/web_fll_gallery2_01_imacsmall.jpg')"></div>
+             <div id="carousel-image-2"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery2/web_fll_gallery2_02_imacsmall.jpg')"></div>
+             <div id="carousel-image-3"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery2/web_fll_gallery2_03_imacsmall.jpg')"></div>
+             <div id="carousel-image-4"class="carousel-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/web_fll/Gallery2/web_fll_gallery2_04_imacsmall.jpg')"></div>
          </div> <!-- /end carousel-images -->
          <div class="carousel-controls">
              <div id="carousel-control-1" class="carousel-control selected"></div>
@@ -123,27 +123,13 @@
  </div>
 
  <div class="container container--no-padding">
-     <section>
-         <div class="group-container">
-             <div class="group group--left">
-                 <div class="module module--2-2">
-                   <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_05_ls.jpg')"></div>
-                 </div>
-             </div>
-             <div class="group group--right">
-                 <div class="module module--2-1 area-dark">
-                   <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_06_mr.jpg')"></div>
-                 </div>
-                 <div class="module module--1-1">
-                   <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_07_ss.jpg')"></div>
-                 </div>
-                 <div class="module module--1-1">
-                   <div class="ratio" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/work/int_pbs/int_pbs_08_ss.jpg')"></div>
-                 </div>
-             </div> <!-- /end group -->
-         </div> <!-- /end group-container -->
-     </section>
- </div> <!-- /end container -->
+
+    <video class="video-js vjs-default-skin" controls poster="<?php echo get_template_directory_uri(); ?>/images/work/web_fll/leadership_mistakes_poster_image.jpg" width="100%" height="100%" >
+            <source src="https://player.vimeo.com/external/129132162.hd.mp4?s=f52155809a57c8520d28a9ab04c3b9fe&profile_id=113" type="video/mp4">
+    </video>
+
+</div> <!-- /end container -->
+
 
 
 
