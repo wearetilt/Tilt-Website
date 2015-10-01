59a9c1404f655577d13479d5cb8fd68c761c5ab0
Jonathan Malyon <jon@wearetilt.com>
2015-10-01 21:24:05 +0100

    BP FLL Stories main film added

--- wp-content/themes/tilt-site/js/main.js
+++ wp-content/themes/tilt-site/js/main.js
@@ -1055,8 +1055,8 @@ if(document.getElementById('header-video-player')){
 			break;
 
 			case "page_bp_fll_stories":
-				var clipVideoSrc = "https://player.vimeo.com/external/140671448.sd.mp4?s=4c8fa29917b77e5bf2cabc4ef37ff99c&profile_id=112";
-				var fullVideoSrc = "https://player.vimeo.com/external/140771096.hd.mp4?s=18276ebc9ead0e5b936f685afac90314&profile_id=113";
+				var clipVideoSrc = "https://player.vimeo.com/external/139330951.hd.mp4?s=3d1c83178ba678c46e7f01baebb21bff&profile_id=113";
+				var fullVideoSrc = "https://player.vimeo.com/external/141101947.hd.mp4?s=cd503eb677f03e6164bee57bccb92c1c&profile_id=113";
 			break;
 
 			case "page_discover_bp":
