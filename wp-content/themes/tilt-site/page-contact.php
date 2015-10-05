d2c7e903380924a9b775570888f08230236a6fbe
wearemiles <miles@wearetilt.com>
2015-10-05 16:22:22 +0100

    more contact stuff

--- wp-content/themes/tilt-site/page-contact.php
+++ wp-content/themes/tilt-site/page-contact.php
@@ -90,7 +90,7 @@ get_header('contact'); ?>
 </script>
 
 <header class="contact-page">
-	<div class="module module--16-5 module--video">
+	<div class="module module--16-5 module--video module--visible">
 			 <video poster="" loop="true" muted="true">
 					 <source src="https://player.vimeo.com/external/141399412.hd.mp4?s=3f1574fa69a9ae469f325e2b05972a6e&profile_id=113" type="video/mp4">
 			 </video>
