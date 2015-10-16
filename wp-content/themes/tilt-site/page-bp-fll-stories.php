ac715e80beef0f5dd89ea8ad48b6b2adab495fdc
Jonathan Malyon <jon@wearetilt.com>
2015-10-16 16:16:22 +0100

    Poster images

--- wp-content/themes/tilt-site/page-bp-fll-stories.php
+++ wp-content/themes/tilt-site/page-bp-fll-stories.php
@@ -14,7 +14,7 @@
 get_header('work-item'); ?>
 
 <div id="video-overlay" class="fullpage-overlay">
-    <video id="overlay-video" controls class="video-js vjs-default-skin vertical-align" poster="<?php echo get_template_directory_uri(); ?>/images/work/mo_integrity/film_poster_image.jpg" width="100%" height="auto">
+    <video id="overlay-video" controls class="video-js vjs-default-skin vertical-align" poster="<?php echo get_template_directory_uri(); ?>/images/work/film_fll/fll_poster.jpg" width="100%" height="auto">
         <source src="https://player.vimeo.com/external/141548149.hd.mp4?s=c38947ea65f3bad06d05e9881fe92ead&profile_id=113" type="video/mp4">
     </video>
     <div id="video-overlay-close"><p class="vertical-align sans-serif">X</p></div>
@@ -26,7 +26,7 @@ get_header('work-item'); ?>
 
         </div>
         <div class="ratio">
-            <video id="header-video-player" class="video-js vjs-default-skin" autoplay loop width="100%" height="100%" >
+            <video id="header-video-player" class="video-js vjs-default-skin" autoplay loop poster="<?php echo get_template_directory_uri(); ?>/images/work/film_fll/fll_poster.jpg" width="100%" height="100%" >
                     <source id="header-video" src="https://player.vimeo.com/external/139330951.hd.mp4?s=3d1c83178ba678c46e7f01baebb21bff&profile_id=113" type="video/mp4">
             </video>
         </div>
