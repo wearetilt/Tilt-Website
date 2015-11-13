9e3b14f70c7c1c54b645103a05e3ea9b74e3afe5
wearemiles <miles@wearetilt.com>
2015-11-13 16:28:41 +0000

    404

--- wp-content/themes/tilt-site/404.php
+++ wp-content/themes/tilt-site/404.php
@@ -7,24 +7,38 @@
  * @since Twenty Fifteen 1.0
  */
 
-get_header(); ?>
+get_header('404'); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="video-overlay" class="fullpage-overlay">
+    <video id="overlay-video" controls class="video-js vjs-default-skin vertical-align" poster="<?php echo get_template_directory_uri(); ?>/images/work/mo_gfk/gfk_poster_2.jpg" width="100%" height="auto">
+        <source src="https://player.vimeo.com/external/141548149.hd.mp4?s=c38947ea65f3bad06d05e9881fe92ead&profile_id=113" type="video/mp4">
+    </video>
+    <div id="video-overlay-close"></div>
+</div>
+<header id="page_404" class="work-item work-item--motion area-dark">
+    <div class="module--video module--header">
+        <div id="header-play" class="header-play">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header -->
+        </div>
+        <div class="ratio">
+            <video id="header-video-player" class="video-js vjs-default-skin" autoplay loop width="100%" height="100%" poster="<?php echo get_template_directory_uri(); ?>/images/work/mo_gfk/gfk_poster_1.jpg">
+                    <source id="header-video" src="https://player.vimeo.com/external/140667746.hd.mp4?s=65dbf2593c9f3bed0c770c497eda1964&profile_id=113" type="video/mp4">
+            </video>
+        </div>
+    </div>
+</header>
+<div id="404-text">
+	<h1>404 - Page doesn't exist</h1>
+	<h1 class="light">The tilt monkey isn't amused about that fact</h1>
+</div>
+</div>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
+<script src="http://vjs.zencdn.net/ie8/1.1.0/videojs-ie8.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/video.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/css/video-js.min.css"></script>
+<link href='https://fonts.googleapis.com/css?family=Merriweather:400,400italic,700,700italic,300,300italic' rel='stylesheet' type='text/css'>
 
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+<?php wp_footer(); ?>
 
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
-<?php get_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/tilt-site/header-404.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width initial-scale=1">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script>
+		window.location = "http://www.google.com/";
+	</script>
+	<![endif]-->
+	<script type="text/javascript">
+		window.directoryURI = "<?php echo get_template_directory_uri(); ?>";
+	</script>
+	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAXrz6wY5cdWYYsdOr041i9Cdb74DjOHns&sensor=false"></script>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<nav>
+        <div id="tilt--logo" class="header-item header-item--logo">
+			<a href="<?php echo site_url(); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/tilt-logo_sm.png" alt="">
+			</a>
+        </div>
+        <a id="menuButton" class="header-item header-item--menu">Menu</a>
+        <a id="closeButton" class="header-item header-item--menu">Close</a>
+
+        <div id="pageMenu" class="menu">
+            <ul class="menu__items">
+                <li class="menu__item"><a href="<?php echo site_url(); ?>/work">Work</a></li>
+                <li class="menu__item"><a href="<?php echo site_url(); ?>/about">About</a></li>
+                <li class="menu__item"><a href="<?php echo site_url(); ?>/team">Team</a></li>
+                <li class="menu__item"><a href="<?php echo site_url(); ?>/news">News</a></li>
+                <li class="menu__item"><a href="<?php echo site_url(); ?>/careers">Careers</a></li>
+                <li class="menu__item"><a href="<?php echo site_url(); ?>/contact">Contact</a></li>
+            </ul>
+
+            <div class="nav-icons">
+				<div id="nav-icon-1" class="nav-icon"><a href="https://www.facebook.com/wearetilt" target="_blank" ></a></div>
+				<div id="nav-icon-2" class="nav-icon"><a href="https://twitter.com/wearetilt" target="_blank" ></a></div>
+				<div id="nav-icon-3" class="nav-icon"><a href="https://vimeo.com/wearetilt/" target="_blank" ></a></div>
+				<div id="nav-icon-4" class="nav-icon"><a href="https://instagram.com/we_are_tilt" target="_blank" ></a></div>
+				<div id="nav-icon-5" class="nav-icon"><a href="https://www.youtube.com/channel/UC0JTRabxxDhTzUUBd9CatiQ" target="_blank" ></a></div>
+				<div id="nav-icon-6" class="nav-icon"><a href="http://bakery.wearetilt.com/" target="_blank" ></a></div>
+				<div class="telephone">
+					<span>Tel: + 44(0)1273 675 700</span>
+				</div>
+			</div> <!-- /end nav-icons -->
+
+        </div> <!-- /end menu -->
+    </nav>
+	<div id="404" class="wrapper"> <!-- Open Wrapper -->
