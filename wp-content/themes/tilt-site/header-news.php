146268869505d8b7d3d9e641bad3f6c25a44469d
Rachel Bilski <rachel@wearetilt.com>
2017-03-28 09:46:21 +0100

    adding the ux thingy

--- wp-content/themes/tilt-site/header-news.php
+++ wp-content/themes/tilt-site/header-news.php
@@ -22,6 +22,17 @@
 	<script type="text/javascript">
 		window.directoryURI = "<?php echo get_template_directory_uri(); ?>";
 	</script>
+	<!-- Hotjar Tracking Code for www.wearetilt.com -->
+<script>
+  (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:456410,hjsv:5};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 	<?php wp_head(); ?>
 </head>
 
