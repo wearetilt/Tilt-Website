1f62a76dbdc757bd2737bba109e9baca63b9e480
Valentin Sampl <valentin.sampl@wearetilt.com>
2017-09-08 17:02:48 +0100

    Remove network, contact page

--- wp-config.php
+++ wp-config.php
@@ -41,16 +41,16 @@ if($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
 	define('FS_METHOD', 'direct');
 }
 
-define( 'COOKIE_DOMAIN', $_SERVER[ 'HTTP_HOST' ] );
+// define( 'COOKIE_DOMAIN', $_SERVER[ 'HTTP_HOST' ] );
 
-define( 'WP_ALLOW_MULTISITE', true );
+// define( 'WP_ALLOW_MULTISITE', true );
 
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', 'tilt.dev');
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
+// define('MULTISITE', true);
+// define('SUBDOMAIN_INSTALL', true);
+// define('DOMAIN_CURRENT_SITE', 'tilt.dev');
+// define('PATH_CURRENT_SITE', '/');
+// define('SITE_ID_CURRENT_SITE', 1);
+// define('BLOG_ID_CURRENT_SITE', 1);
 
 
 /**#@+
--- wp-content/themes/tilt-site/page-contact.php
+++ wp-content/themes/tilt-site/page-contact.php
@@ -21,6 +21,7 @@ get_header('contact'); ?>
 	google.maps.event.addDomListener(window, 'load', init);
 
 	officeLocation = new google.maps.LatLng(50.8319498, -0.1392226);
+	officeLocationCA = new google.maps.LatLng(49.323680, -123.103750);
 
 	brightonStation = new google.maps.LatLng(50.829262, -0.141058);
 	hoveStation = new google.maps.LatLng(50.835210, -0.17066);
@@ -41,7 +42,7 @@ get_header('contact'); ?>
 			// How you would like to style the map.
 			// This is where you would paste any style found on Snazzy Maps.
 			styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#292929"}]},{"featureType":"transit.line","elementType":"geometry.stroke","stylers":[{"color":"#292929"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-			};
+		};
 
 		// Get the HTML DOM element that will contain your map
 		// We are using a div with id="map" seen below in the <body>
@@ -127,58 +128,134 @@ get_header('contact'); ?>
 
 		}
 
+
+		var mapOptions = {
+			// How zoomed in you want the map to start at (always required)
+			zoom: 14,
+
+			// The latitude and longitude to center the map (always required)
+			center: officeLocationCA, // New York
+			disableDefaultUI: false,
+			scrollwheel: false,
+			// How you would like to style the map.
+			// This is where you would paste any style found on Snazzy Maps.
+			styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"color":"#292929"}]},{"featureType":"transit.line","elementType":"geometry.stroke","stylers":[{"color":"#292929"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+		};
+
+		var mapElement = document.getElementById('map2');
+
+		// Create the Google Map using our element and options defined above
+		var map = new google.maps.Map(mapElement, mapOptions);
+
+		var myMarker = 0;
+
+		var newMarker = {
+			url: 'images/marker.png',
+			size: new google.maps.Size(39, 39),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(19, 19),
+			scaledSize: new google.maps.Size(39, 39)
+		}
+
+		var image = new google.maps.MarkerImage('<?php echo get_template_directory_uri(); ?>/images/marker.png',
+			new google.maps.Size(25,25), //size
+			new google.maps.Point(1,1), //origin
+			null,
+			new google.maps.Size(25,25)
+		);
+
+		myMarker = new google.maps.Marker({
+				flat: true,
+				icon: image,
+				map: map,
+				optimized: false,
+				position: officeLocationCA,
+				title: 'Our Office',
+				visible: true
+		});
+
+
 	}
 </script>
 
 
-<div class="container container--double-side-pad area-dark contact--page">
-	<section class="text-section text-section--centre">
-		<h2 class="contact-title"><span class="light">Call us</span></h2>
-		<h2 class="contact-phone"><span class="light"><a title="Call tilt" href="tel:+441273675700">+44(0)1273 675 700</a></span></h2>
-	
-		<h2 class="contact-title"><span class="light">Jobs &amp; Internships</span></h2>
-		<a class="contact-email" href="mailto:recruitment@wearetilt.com">recruitment@wearetilt.com</a>
-		<h2 class="contact-title"><span class="light">New projects</span></h2>
-		<a class="contact-email" href="mailto:jonathan.helm@wearetilt.com">jonathan.helm@wearetilt.com</a>
-		<h2 class="contact-title"><span class="light">General Enquiries</span></h2>
-		<a class="contact-email" href="mailto:studio@wearetilt.com">studio@wearetilt.com</a>
+<div class="container area-dark contact--page">
+	<section class="contact-section">
 
+		<div class="contact-section-info">
 
-		<!--
-<div class="worksheet-button-container">
-			<p>Try our handy worksheet to help you define your project.</p>
-			<a id="form_open" class="button" href="#">Open worksheet</a>
-			<a id="form_close" class="button" href="#">Close worksheet</a>
+			<h2>Brighton HQ</h2>
+
+			<div class="contact-title"><span class="light">Call us</span></div>
+			<div class="contact-phone"><span class="light"><a title="Call tilt" href="tel:+441273675700">+44(0)1273 675 700</a></span></div>
+		
+			<div class="contact-title"><span class="light">Jobs &amp; Internships</span></div>
+			<a class="contact-email" href="mailto:recruitment@wearetilt.com">recruitment@wearetilt.com</a>
+			<div class="contact-title"><span class="light">New projects</span></div>
+			<a class="contact-email" href="mailto:jonathan.helm@wearetilt.com">jonathan.helm@wearetilt.com</a>
+			<div class="contact-title"><span class="light">General Enquiries</span></div>
+			<a class="contact-email" href="mailto:studio@wearetilt.com">studio@wearetilt.com</a>
+
+			<div class="contact-address">
+				41 New England Street, Brighton,<br>
+				East Sussex, BN1 4GQ. UK
+			</div>
+
+			<div class="contact-directions">
+				<a class="cube--link" target="_blank" href="https://www.google.co.uk/maps/dir//50.8319498,-0.1392226/@50.8321729,-0.1393055,19.51z/data=!4m2!4m1!3e0?hl=en">
+					<div class="cube">
+						<div class="cube--front">
+							<p class="sans-serif">Get directions</p>
+						</div>
+						<div class="cube--top">
+							<p class="sans-serif">Get directions</p>
+						</div>
+					</div>
+				</a>
+			</div>
+
 		</div>
--->
+
+		<div class="map" id="map"></div>
+
+	</section> <!-- /end text-section -->
+
+	<section class="contact-section">
+
+		<div class="contact-section-info">
+
+			<h2>Vancouver</h2>
+		
+			<div class="contact-title"><span class="light">Call us</span></div>
+			<div class="contact-phone"><span class="light"><a title="Call tilt" href="tel:+17788356414">+1 (778) 835-6414</a></span></div>
+		
+			<div class="contact-title"><span class="light">Email</span></div>
+			<a class="contact-email" href="mailto:bill.mackenzie@wearetilt.ca">bill.mackenzie@wearetilt.ca</a>
+
+			<div class="contact-address">
+			1061 Marine Drive,  Suite 216 <br>
+			North Vancouver, BC  CANADA V7P 1S6
+			</div>
+
+			<div class="contact-directions">
+				<a class="cube--link" target="_blank" href="https://www.google.co.uk/maps/dir//1061+Marine+Dr+%23216,+North+Vancouver,+BC+V7P+3M6,+Canada/@49.3237127,-123.1388464,13z/data=!3m1!4b1!4m9!4m8!1m0!1m5!1m1!1s0x548671d288618c47:0xa410c94e2a818608!2m2!1d-123.1037413!2d49.3236621!3e0?hl=en">
+					<div class="cube">
+						<div class="cube--front">
+							<p class="sans-serif">Get directions</p>
+						</div>
+						<div class="cube--top">
+							<p class="sans-serif">Get directions</p>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<div class="map" id="map2"></div>
+		
+
 	</section> <!-- /end text-section -->
 </div> <!-- /end container -->
-
-
-<div class="container container--no-padding">
-	<div id="map">
-	</div>
-	<div class="map-overlay">
-		<h3 class="light marker">Call us</h3>
-		<span>+44(0)1273 675 700</span>
-		<h3 class="light marker">Our address</h3>
-		<span>Unit 1, Pullman haul,</span>
-		<span>41 New England Street,</span>
-		<span>Brighton, East Sussex,</span>
-		<span>BN1 4GQ, UK</span>
-		<a href="https://www.google.co.uk/maps/dir//50.8319498,-0.1392226/@50.8321729,-0.1393055,19.51z/data=!4m2!4m1!3e0?hl=en">
-			<div class="cube">
-					<div class="cube--front">
-						<p class="sans-serif">Get Directions</p>
-					</div>
-					<div class="cube--top">
-						<p class="sans-serif">Get Directions</p>
-					</div>
-			</div>
-		</a>
-	</div>
-
-</div>
 
 </div>
 
