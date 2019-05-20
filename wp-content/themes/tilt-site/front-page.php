ebb8b8d029e6d637953b5a63ad905e2cea7403ab
joshyhud <joshhudsonmusic@gmail.com>
2019-05-20 17:53:51 +0100

    homepage build

--- wp-content/themes/tilt-site/front-page.php
+++ wp-content/themes/tilt-site/front-page.php
@@ -10,154 +10,184 @@
 
 get_header('home'); ?>
 
-<?php
- /*
-$args = array(
-	 'posts_per_page' => 1,
-	 'order_by' => 'date',
-	 'post_type' => 'post',
-	 'post_status' => 'publish',
-	 'category'         => '-9',
- );
-
- $posts_array = get_posts( $args );
-
- $post = $posts_array[0];
- $postID = $post->ID;
-
- if (has_post_thumbnail( $postID ) ){
-	 $image = wp_get_attachment_image_src( get_post_thumbnail_id( $postID ), 'single-post-thumbnail' );
- }
-*/
-
-
-	$args = array(
-		 'posts_per_page' => 3,
-		 'order_by' => 'date',
-		 'post_type' => 'post',
-		 'post_status' => 'publish',
-		 'category' => '-9',
-
-	 );
-
-	 $posts_array = get_posts( $args );
-	 $post = $posts_array[0];
-	 $postID = $post->ID;
-
-	 if (has_post_thumbnail( $postID ) ){
-		 $image = wp_get_attachment_image_src( get_post_thumbnail_id( $postID ), 'single-post-thumbnail' );
-	 }
-
-	 $filmposts = get_posts( $args );
-	 $i = 0;
-	 $j = 0;
-	 $k = 0;
-
-
-
-
-?>
 
 <header id="home-page" class="work-item work-item--motion area-dark">
-	<div class="module--video module--header">
-		<div class="ratio">
-			<div class="container container--header strapline-container">
-				<h1>We Are <strong id="strapline-text">Tilt</strong></h1>
-				<a class="cube--link" href="<?php echo site_url(); ?>/work">
-					<div class="cube">
-						<div class="cube--front cube--front__no-bg">
-							<p class="sans-serif">See our work</p>
-						</div>
-						<div class="cube--top cube--top__no-bg">
-							<p class="sans-serif">See our work</p>
-						</div>
-					</div>
-				</a>
-			</div>
-			<video id="header-video-player" class="video-js vjs-default-skin" autoplay loop muted poster="<?php echo get_template_directory_uri(); ?>/images/home/header_poster_image.jpg">
-					<source id="header-video" src="https://player.vimeo.com/external/144378575.hd.mp4?s=9126a5c2a202cb2d55ecf33fefe42a3a&profile_id=113" type="video/mp4">
-			</video>
-		</div>
-		<a class="scroll-down" href="#"><span><p>Scroll down</p></span></a>
-	</div>
-</header>
+  <div class="module--video module--header">
+    <div class="ratio">
+      <div class="container container--header strapline-container">
+        <h1>We Are <strong id="strapline-text">Tilt</strong></h1>
+        <a class="cube--link" href="<?php echo site_url(); ?>/work">
+          <div class="cube">
+            <div class="cube--front cube--front__no-bg">
+              <p class="sans-serif">See our work</p>
+            </div>
+            <div class="cube--top cube--top__no-bg">
+              <p class="sans-serif">See our work</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <video id="header-video-player" class="video-js vjs-default-skin" autoplay loop muted poster="<?php echo get_template_directory_uri(); ?>/images/home/header_poster_image.jpg">
+        <source id="header-video" src="https://player.vimeo.com/external/144378575.hd.mp4?s=9126a5c2a202cb2d55ecf33fefe42a3a&profile_id=113" type="video/mp4">
+        </video>
+      </div>
+      <a class="scroll-down" href="#"><span><p>Scroll down</p></span></a>
+    </div>
+  </header>
 
-<div id="header-overlay" class="container container--header area-dark">
+  <div id="header-overlay" class="container container--header area-dark">
     <a id="blog_button" class="button button--no-border" href="<?php echo site_url(); ?>/submotion-orchestra">About this video</a>
-	<div class="text-container">
-		<p class="first-para sans-serif"><strong class="highlight">CRAFTED DIGITAL EXPERIENCES</strong> FROM AN OBSESSIVE BUNCH OF STRATEGISTS, ARTISTS, FILMMAKERS, ANIMATORS, PRODUCERS, ILLUSTRATORS, WRITERS, CODERS AND CREATIVES.</p>
+    <div class="text-container">
+      <p class="first-para sans-serif"><strong class="highlight">CRAFTED DIGITAL EXPERIENCES</strong> FROM AN OBSESSIVE BUNCH OF STRATEGISTS, ARTISTS, FILMMAKERS, ANIMATORS, PRODUCERS, ILLUSTRATORS, WRITERS, CODERS AND CREATIVES.</p>
 
 
-		<a class="cube--link" href="<?php echo site_url(); ?>/about/">
-			<div class="cube">
-				<div class="cube--front">
-					<p class="sans-serif">More about us</p>
-				</div>
-				<div class="cube--top">
-					<p class="sans-serif">More about us</p>
-				</div>
-			</div>
-		</a>
-	</div>
-</div>
+      <a class="cube--link" href="<?php echo site_url(); ?>/about/">
+        <div class="cube">
+          <div class="cube--front">
+            <p class="sans-serif">More about us</p>
+          </div>
+          <div class="cube--top">
+            <p class="sans-serif">More about us</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
 
-<div class="container container--no-padding area-dark">
+  <main class="main-work-content">
 
-<?php if(have_rows('front_page_content') ) { 
+    <?php
 
-		if(get_row_layout() == 'frontpage_work_items') {
-		$left_container = get_sub_field('work_item_left');
-		$right_container = get_sub_field('work_item_right');
+    if( have_rows('front_page_content') ):
+      while ( have_rows('front_page_content') ) : the_row();
 
-?>
-<div class="left-item">
-<?php echo $left_container; ?>
-</div>
+        ?>
 
-<?php 
-}
-}
-?>
-</div>
-<!-- /end container -->
+        <?php 
+        if(get_row_layout() == 'frontpage_work_items'):
+
+          $left_project = get_sub_field('work_item_left');
+          $right_project = get_sub_field('work_item_right');
+          $post_image = get_the_post_thumbnail();
+          $bold_left = get_post_meta($left_project->ID, 'work_items_0_title_bold');
+          $bold_right = get_post_meta($right_project->ID, 'work_items_0_title_bold');
+          ?>
+          <div class="work-item area-dark" >
+
+            <div class="left_container project_container"> 
+              <a href='<?php echo get_permalink($left_project); ?>'>
+                <?php if($post_image != '') {
+                  echo $post_image;
+                } else { ?>
+                  <img src="/wp-content/themes/tilt-site/images/404_poster_loop.jpg"/>
+                <?php } ?>
+              </a>
+
+              <div class="related-links">
+                <h2 class="entry-title"><?php echo $bold_left[0]; ?></h2>
+               <p><?php echo $left_project->post_title;  ?></p>
+              </div>
+            </div>
+
+            <div class="right_container project_container"> 
+              <a href='<?php echo get_permalink($right_project); ?>'>
+                <?php if($post_image != '') {
+                  echo $post_image;
+                } else { ?>
+                  <img src="/wp-content/themes/tilt-site/images/404_poster_loop.jpg"/>
+                <?php } ?>
+              </a>
+
+              <div class="related-links">
+                <h2 class="entry-title"><?php echo $bold_right[0]; ?></h2>
+               <p><?php echo $right_project->post_title;  ?></p>
+            </div>
+          </div>
+
+          </div>
+
+    <?php 
+
+      elseif(get_row_layout() == 'see_all'):
+        $our_work = get_sub_field('our_work');
+        ?>
+          <div class=" goto_projects area-dark"> 
+            <div class="our_work_button">
+            <?php echo $our_work; ?> 
+          </div>
+          </div>
 
 
-<?php get_footer(); ?>
 
-<script type="text/javascript">
+    <?php
 
-	var wordArray = ['Thinkers', 'Crafters', 'Grafters', 'Tilt', 'Time-Travellers', 'Navigators', 'Gymnasts', 'Firestarters', 'Geeks', 'Tilt', 'All Ears', 'Entertainers', 'Tea Drinkers'];
-	var maxLoops = wordArray.length;
-	var counter = 0;
+      elseif(get_row_layout() == 'news_row') :
+        if(have_rows('news_items')) :
+    ?>
+          <div class="news-line area-dark"> 
+    <?php
+          while(have_rows('news_items')): the_row();
+            $news = get_sub_field('news_item');
+    ?>
+
+              <div class="news-item">
+                <p>News</p>
+                <?php echo $news->post_title; ?>
+              </div>
+
+    <?php 
+          endwhile;
+    ?>
+          </div>
+    <?php
+        endif;
+      endif;
+
+    endwhile;
+    else :
+    // no rows found
+    endif;
+    ?>
+
+  </main>
 
 
-	jQuery(document).ready(function(){
+  <?php get_footer(); ?>
 
-	(function next() {
+  <script type="text/javascript">
 
-	    if (counter < maxLoops) {
+    var wordArray = ['Thinkers', 'Crafters', 'Grafters', 'Tilt', 'Time-Travellers', 'Navigators', 'Gymnasts', 'Firestarters', 'Geeks', 'Tilt', 'All Ears', 'Entertainers', 'Tea Drinkers'];
+    var maxLoops = wordArray.length;
+    var counter = 0;
 
-		    setTimeout(function() {
 
-	        	jQuery('#strapline-text').fadeOut(500, function(){
+    jQuery(document).ready(function(){
 
-		        	document.getElementById('strapline-text').innerHTML = wordArray[counter];
-					jQuery('#strapline-text').fadeIn(500);
-					next();
-					counter++;
-					if(counter == maxLoops) { counter = 0; }
+      (function next() {
 
-	        	});
+        if (counter < maxLoops) {
 
-			}, 2000);
+          setTimeout(function() {
 
-	    } else {
-		    counter = 0;
-		    next();
-	    }
+            jQuery('#strapline-text').fadeOut(500, function(){
 
-	})();
+              document.getElementById('strapline-text').innerHTML = wordArray[counter];
+              jQuery('#strapline-text').fadeIn(500);
+              next();
+              counter++;
+              if(counter == maxLoops) { counter = 0; }
 
-	});
+            });
 
-</script>
+          }, 2000);
+
+        } else {
+          counter = 0;
+          next();
+        }
+
+      })();
+
+    });
+
+  </script>
