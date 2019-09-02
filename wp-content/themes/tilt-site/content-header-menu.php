43c4a4ec978517e596373547b7436cf433a041cc
joshyhud <joshhudsonmusic@gmail.com>
2019-09-02 08:41:55 +0100

    amend back to work menu item

--- wp-content/themes/tilt-site/content-header-menu.php
+++ wp-content/themes/tilt-site/content-header-menu.php
@@ -1,7 +1,7 @@
 <?php $blog_id = get_current_blog_id();?>
 <?php 
     $body_class = get_body_class();
-    if(in_array('page-template-new-work', $body_class)){
+    if(in_array('single-work_item', $body_class)){
 ?>
 <a id="backButton" class="header-item header-item--menu" href="/work">Back to work</a>
 <?php } ?>
