4645fc9449995189e65851375cff5ed139fe9b8c
josh <joshhudsondev@gmail.com>
2021-09-08 17:46:40 +0100

    added styling amend for mobile and desktop image headers on work items

--- wp-content/themes/tilt-site/header.php
+++ wp-content/themes/tilt-site/header.php
@@ -45,6 +45,35 @@
 	<!-- Minified Cookie Consent served from our CDN -->
 	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
 
+
+	<?php
+	#twitter cards hack
+	if(is_single() || is_page()) {
+	  $twitter_url    = get_permalink();
+	 $twitter_title  = get_the_title();
+	 $twitter_desc   = get_the_excerpt();
+	   $twitter_thumbs = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), full );
+	    $twitter_thumb  = $twitter_thumbs[0];
+	      if(!$twitter_thumb) {
+	      $twitter_thumb = 'http://www.gravatar.com/avatar/8eb9ee80d39f13cbbad56da88ef3a6ee?rating=PG&size=75';
+	    }
+	  $twitter_name   = str_replace('@', '', get_the_author_meta('twitter'));
+	?>
+	<meta name="twitter:card" value="summary" />
+	<meta name="twitter:url" value="<?php echo $twitter_url; ?>" />
+	<meta name="twitter:title" value="<?php echo $twitter_title; ?>" />
+	<meta name="twitter:description" value="<?php echo $twitter_desc; ?>" />
+	<meta name="twitter:image" value="<?php echo $twitter_thumb; ?>" />
+	<meta name="twitter:site" value="@wearetilt" />
+	<?
+	  if($twitter_name) {
+	?>
+	<meta name="twitter:creator" value="@wearetilt" />
+	<?
+	  }
+	}
+	?>
+
 </head>
 
 
