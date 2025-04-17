0db2859d7c4fb94057026f8522f593d31c6b127a
sas <sas@wearetilt.com>
2025-04-17 13:19:42 +0100

    new homepage copy

--- wp-content/themes/tilt-site/front-page.php
+++ wp-content/themes/tilt-site/front-page.php
@@ -39,7 +39,7 @@ get_header('home'); ?>
 <div id="header-overlay" class="container container--header area-dark">
   <a id="blog_button" class="button button--no-border" href="<?php echo site_url(); ?>/submotion-orchestra">About this video</a>
   <div class="text-container">
-    <p class="sans-serif"><strong class="highlight">A thinking & doing studio</strong> that creates flagship learning & marketing experiences for complex organisations. From professional services and engineering, to global drinks brands and the UN.</p>
+    <p class="sans-serif"><strong class="highlight">A thinking and doing creative studio</strong>  that solves complex challenges and drives meaningful change by helping clients navigate the invisible forces that shape behaviour.</p>
 
 
     <a class="cube--link" href="<?php echo site_url(); ?>/about/">
