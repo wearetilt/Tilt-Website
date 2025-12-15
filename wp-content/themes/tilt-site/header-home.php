6024f3e0b51566189bca08be9bb9d4acfe89506f
Colette Wilson Wyatt <hello@colette.dev>
2025-12-15 16:23:47 +0000

    add slick css

--- wp-content/themes/tilt-site/header-home.php
+++ wp-content/themes/tilt-site/header-home.php
@@ -14,6 +14,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width initial-scale=1">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/slick.css"/>
 	<!--[if lt IE 9]>
 	<script>
 		location.replace('http://wearetilt.com/home-browser-upgrade');
